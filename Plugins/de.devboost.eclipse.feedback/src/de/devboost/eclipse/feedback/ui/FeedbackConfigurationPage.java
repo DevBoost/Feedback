cda28a6f24f1e6d8cad00e166fe7b7ec68174e71
Mirko Seifert <mirko.seifert@devboost.de>
2012-08-14 16:23:01 +0200

    added TODO items

--- Plugins/de.devboost.eclipse.feedback/src/de/devboost/eclipse/feedback/ui/FeedbackConfigurationPage.java
+++ Plugins/de.devboost.eclipse.feedback/src/de/devboost/eclipse/feedback/ui/FeedbackConfigurationPage.java
@@ -9,6 +9,7 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
+// TODO fix texts
 class FeedbackConfigurationPage extends WizardPage {
 
 	private static final String PAGE_TITLE = "DevBoost Feedback Configuration";
