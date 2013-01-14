25ec5395f1014c2ca80a88ceac14cc0443958380
Mirko Seifert <mirko.seifert@devboost.de>
2013-01-14 16:50:40 +0100

    added missing file IConfigurationConstants

---
+++ Core/Plugins/de.devboost.eclipse.feedback/src/de/devboost/eclipse/feedback/IConfigurationConstants.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2013
+ * DevBoost GmbH, Berlin, Amtsgericht Charlottenburg, HRB 140026
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   DevBoost GmbH - Berlin, Germany
+ *      - initial API and implementation
+ ******************************************************************************/
+package de.devboost.eclipse.feedback;
+
+public interface IConfigurationConstants {
+
+	public String CONFIG_FILE_NAME = ".devboost-open-source-tools";
+	public String SYSTEM_PROPERTY_USER_DIR = "user.home";
+	
+	public String KEY_GUID = "guid";
+	public String KEY_EMAIL = "email";
+	public String KEY_REGISTER_INSTALLATION = "register_installation";
+	public String KEY_SEND_ERROR_REPORTS = "send_error_reports";
+	public String KEY_DATE = "date";
+}
